477e7495a8eb892c1bce49a7cb48e1d351d96644
EmiCon97 <derleuchtturm97@gmail.com>
2024-01-23 21:26:21 -0300

    feature: diary working, some styles pending migration

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 // ** Reducers
 import chat from 'src/store/apps/chat';
+import diary from 'src/store/diary';
 import user from 'src/store/apps/user';
 
 export const store = configureStore({
   reducer: {
     user,
+    diary,
     chat,
   },
   middleware: (getDefaultMiddleware) =>
