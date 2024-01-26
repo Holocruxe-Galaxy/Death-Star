d19ac97b0729371923bc27a33a06f3e70edc5381
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2024-01-26 19:42:11 -0300

    fix: arreglando bug a la hora de crear cuentas

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -33,6 +33,7 @@ import BlankLayout from 'src/@core/layouts/BlankLayout';
 
 // ** Hooks
 import { useSettings } from 'src/@core/hooks/useSettings';
+import { useAuth } from 'src/hooks/useAuth';
 
 // ** Styled Components
 const RegisterIllustrationWrapper = styled(Box)<BoxProps>(({ theme }) => ({
@@ -95,7 +96,10 @@ const LinkStyled = styled(Link)(({ theme }) => ({
 const Register = () => {
   // ** States
   const [showPassword, setShowPassword] = useState<boolean>(false);
-
+  const { logout } = useAuth();
+  const handleLogout = () => {
+    logout();
+  };
   // ** Hooks
   const theme = useTheme();
   const { settings } = useSettings();
@@ -249,160 +253,30 @@ const Register = () => {
               </Typography>
             </Box>
             <Box component="div" sx={{ mb: 6 }}>
-              <TypographyStyled variant="h5">
-                Adventure starts here 🚀
+              <TypographyStyled variant="h4">
+                Aquí iría el onboarding
+              </TypographyStyled>
+              <br></br>
+              <br></br>
+              <TypographyStyled variant="h6">
+                Desloguearse para volver al Login
               </TypographyStyled>
               <Typography variant="body2">
-                Make your app management easy and fun!
+                si se quiere ir al /testin es necesario tener completo el
+                onboarding en el otro repo
               </Typography>
             </Box>
-            <form
-              noValidate
-              autoComplete="off"
-              onSubmit={(e) => e.preventDefault()}
-            >
-              <TextField
-                autoFocus
-                fullWidth
-                sx={{ mb: 4 }}
-                label="Username"
-                placeholder="johndoe"
-              />
-              <TextField
-                fullWidth
-                label="Email"
-                sx={{ mb: 4 }}
-                placeholder="user@email.com"
-              />
-              <FormControl fullWidth>
-                <InputLabel htmlFor="auth-login-v2-password">
-                  Password
-                </InputLabel>
-                <OutlinedInput
-                  label="Password"
-                  id="auth-login-v2-password"
-                  type={showPassword ? 'text' : 'password'}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        edge="end"
-                        onMouseDown={(e) => e.preventDefault()}
-                        onClick={() => setShowPassword(!showPassword)}
-                      >
-                        <Icon
-                          icon={
-                            showPassword
-                              ? 'mdi:eye-outline'
-                              : 'mdi:eye-off-outline'
-                          }
-                        />
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                />
-              </FormControl>
 
-              <FormControlLabel
-                control={<Checkbox />}
-                sx={{
-                  mb: 4,
-                  mt: 1.5,
-                  '& .MuiFormControlLabel-label': { fontSize: '0.875rem' },
-                }}
-                label={
-                  <>
-                    <Typography variant="body2" component="span">
-                      I agree to{' '}
-                    </Typography>
-                    <LinkStyled href="/" onClick={(e) => e.preventDefault()}>
-                      privacy policy & terms
-                    </LinkStyled>
-                  </>
-                }
-              />
-              <Button
-                fullWidth
-                size="large"
-                type="submit"
-                variant="contained"
-                sx={{ mb: 7 }}
-              >
-                Sign up
-              </Button>
-              <Box
-                component="div"
-                sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  flexWrap: 'wrap',
-                  justifyContent: 'center',
-                }}
-              >
-                <Typography sx={{ mr: 2, color: 'text.secondary' }}>
-                  Already have an account?
-                </Typography>
-                <Typography
-                  href="/login"
-                  component={Link}
-                  sx={{ color: 'primary.main', textDecoration: 'none' }}
-                >
-                  Sign in instead
-                </Typography>
-              </Box>
-              <Divider
-                sx={{
-                  '& .MuiDivider-wrapper': { px: 4 },
-                  mt: (theme) => `${theme.spacing(5)} !important`,
-                  mb: (theme) => `${theme.spacing(7.5)} !important`,
-                }}
-              >
-                or
-              </Divider>
-              <Box
-                component="div"
-                sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-              >
-                <IconButton
-                  href="/"
-                  component={Link}
-                  sx={{ color: '#497ce2' }}
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <Icon icon="mdi:facebook" />
-                </IconButton>
-                <IconButton
-                  href="/"
-                  component={Link}
-                  sx={{ color: '#1da1f2' }}
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <Icon icon="mdi:twitter" />
-                </IconButton>
-                <IconButton
-                  href="/"
-                  component={Link}
-                  onClick={(e) => e.preventDefault()}
-                  sx={{
-                    color: (theme) =>
-                      theme.palette.mode === 'light' ? '#272727' : 'grey.300',
-                  }}
-                >
-                  <Icon icon="mdi:github" />
-                </IconButton>
-                <IconButton
-                  href="/"
-                  component={Link}
-                  sx={{ color: '#db4437' }}
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <Icon icon="mdi:google" />
-                </IconButton>
-              </Box>
-            </form>
+            <Button
+              onClick={handleLogout}
+              fullWidth
+              size="large"
+              type="submit"
+              variant="contained"
+              sx={{ mb: 7 }}
+            >
+              Cerrar sesión
+            </Button>
           </BoxWrapper>
         </Box>
       </RightWrapper>
