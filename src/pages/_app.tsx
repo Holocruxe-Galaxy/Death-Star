73a350a0b2bf9b39272a3c45cbb02d9473826cd3
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2024-02-22 13:12:15 -0300

    Add react-hotjar for tracking user behavior

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import Head from 'next/head';
 import { Router } from 'next/router';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 // ** Store Imports
 import { store } from 'src/store';
@@ -28,6 +29,7 @@ import themeConfig from 'src/configs/themeConfig';
 
 // ** Third Party Import
 import { Toaster } from 'react-hot-toast';
+import { hotjar } from 'react-hotjar';
 
 // ** Component Imports
 import UserLayout from 'src/layouts/UserLayout';
@@ -127,6 +129,22 @@ const App = (props: ExtendedAppProps) => {
     <UserProvider>
       <Provider store={store}>
         <CacheProvider value={emotionCache}>
+          {/* Hotjar Tracking Code for Login */}
+          <Script
+            id="hotjar"
+            dangerouslySetInnerHTML={{
+              __html: `
+                      (function(h,o,t,j,a,r){
+                      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                      h._hjSettings={hjid:3877312,hjsv:6};
+                      a=o.getElementsByTagName('head')[0];
+                      r=o.createElement('script');r.async=1;
+                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                      a.appendChild(r);
+                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                      `,
+            }}
+          ></Script>
           <Head>
             <title>{`${themeConfig.templateName} - Una experiencia única`}</title>
             <meta
