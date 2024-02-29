0ca8ac91fb7661a28600f2599d94a30ed01d977e
Luis Mera <luismera86@gmail.com>
2024-02-29 16:53:48 -0300

    Add NewsShareCard and fix styles

--- src/components/NewsCard/NewsInfoCard.tsx
+++ src/components/NewsCard/NewsInfoCard.tsx
@@ -7,20 +7,23 @@ interface NewsInfoCardProps {
 }
 
 export const NewsInfoCard = ({ title, description, image }: NewsInfoCardProps) => {
+  const mensaje = 'Hola mundo';
+
+  console.log(mensaje.length);
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: 400 }}>
       <CardActionArea>
-        <CardMedia component="img" height="140" image={image} alt="green iguana" />
+        <CardMedia component="img" height="200" image={image} alt="green iguana" />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {description}
+            {description.length < 200 ? description : description.slice(0, 200) + '...'}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <Button variant="contained" sx={{ borderRadius: 0 }} fullWidth>
+      <Button variant="contained" sx={{ borderRadius: 0, height: 60 }} fullWidth>
         MAS INFO
       </Button>
     </Card>
